a27a71093ac52a712bc03260643a037e83e2fd00
Chris Johnston <cjohndesign@gmail.com>
2018-04-27 02:07:17 -0400

    48 Hour Commit

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
+// Define container
 
 var userContainer = document.getElementById("user-info");
+
+// Get data on all users
 
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'http://challenge-dev.starmarkcloud.com/users');
 ourRequest.onload = function() {
+  var ourData = JSON.parse(ourRequest.responseText);
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
-    var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
   } else {
     console.log("We connected to the server, but it returned an error.");
@@ -15,94 +18,115 @@ ourRequest.onload = function() {
   
 };
 
-
 ourRequest.onerror = function() {
   console.log("Connection error");
 };
 
 ourRequest.send();
 
+
+// Create HTML for all user cards
+
 function renderHTML(data) {
 
+  json.sort(function(a, b){
+    return a.id - b.id;
+});
+
+  userContainer.innerHTML = ""
   var htmlString = "";
 
   for (i = 0; i < data.length; i++) {
-htmlString += "<div class='col-md-6'><div class='user-card'><div class='col-md-3'>" +
+htmlString += "<div class='col-md-6'><div id='" + data[i].id + "' class='user-card'><div class='col-md-4'>" +
               "<img class='avatar clip-circle ' src='" + data[i].avatar + "'>" +
-              "</div><div class='col-md-9'>" +
-              "<h3>" + 
+              "</div><div class='col-md-8'>" +
+              "<h4>" + 
                 data[i].firstName + " " + data[i].lastName + 
-              "</h3>" + 
-              "<li>"; 
+              "</h4>" + 
+              "<ul><li>"; 
 
               if (data[i].phone !== "") {
-                htmlString += data[i].phone;
+                htmlString += "Phone: " + data[i].phone;
               } else {
                 htmlString += "Phone number unavailable";
               };
                               
               htmlString += "</li><li>";
               if (data[i].email !== "") {
-                htmlString += data[i].email;
+                htmlString += "Email: " + data[i].email;
               } else {
                 htmlString += "Email address unavailable";
               };
-                              
-              htmlString += "</li>";
+
+              htmlString += "</li><li>";
+              if (data[i].address.city !== "") {
+                htmlString += "City: " + data[i].address.city;
+              } else {
+                htmlString += "Location address unavailable";
+              };
+                                       
+              htmlString += "</li><li> <a href='#' id='" + data[i].id + "' >View User</a></li> </ul>";
               
-              htmlString += '</div></div></div>';
+              htmlString += "</div></div></div>";
               
               }
 
   
   userContainer.insertAdjacentHTML('beforeend', htmlString);
-}
+};
 
 
-
+// Filter results
 
 $("#filterForm").submit(function(e) {
     
     e.preventDefault();
     var ourData = JSON.parse(ourRequest.responseText);
     var city = document.getElementById("city").value;
-    console.log(city);
     filter(ourData);
-
 
 
 function filter(data) {
 
+  userContainer.innerHTML = "";
   var htmlString = "";
 
   for (i = 0; i < data.length; i++) {
     var currentCity = data[i].address.city;
-    console.log(currentCity);
     if (currentCity == city) {
-                htmlString += "<div class='col-md-6'><div class='user-card'><div class='col-md-3'>" +
+htmlString += "<div class='col-md-6'><div id='" + data[i].id + "' class='user-card'><div class='col-md-4'>" +
               "<img class='avatar clip-circle ' src='" + data[i].avatar + "'>" +
-              "</div><div class='col-md-9'>" +
-              "<h3>" + 
+              "</div><div class='col-md-8'>" +
+              "<h4>" + 
                 data[i].firstName + " " + data[i].lastName + 
-              "</h3>" + 
-              "<li>"; 
+              "</h4>" + 
+              "<ul><li>"; 
 
               if (data[i].phone !== "") {
-                htmlString += data[i].phone;
+                htmlString += "Phone: " + data[i].phone;
               } else {
                 htmlString += "Phone number unavailable";
               };
                               
               htmlString += "</li><li>";
               if (data[i].email !== "") {
-                htmlString += data[i].email;
+                htmlString += "Email: " + data[i].email;
               } else {
                 htmlString += "Email address unavailable";
               };
-                              
-              htmlString += "</li>";
+
+              htmlString += "</li><li>";
+              if (data[i].address.city !== "") {
+                htmlString += "City: " + data[i].address.city;
+              } else {
+                htmlString += "Location address unavailable";
+              };
+                                       
+              htmlString += "</li><li> <a href='#' id='" + data[i].id + "' >View User</a></li> </ul>";
               
-              htmlString += '</div></div></div>';
+              htmlString += "</div></div></div>";
+              
+              
     } else {
       console.log("Not in that city");
     };
@@ -112,6 +136,143 @@ function filter(data) {
   userContainer.insertAdjacentHTML('beforeend', htmlString);
 }
 });
+
+
+// Open specific card
+
+var expandCard = function(e, f) {
+
+    userContainer.innerHTML = "";
+
+    var myGuy = e[f];
+
+    var myGuyHTML = "";
+
+    var htmlString = "";
+
+    var tags = "";
+
+
+    var userTags = function() {
+      for (i = 0; i < myGuy.tags.length; i++) {
+        if (i < myGuy.tags.length - 1) {
+        tags += myGuy.tags[i] + ", "
+        } else {
+        tags += myGuy.tags[i]
+        };
+      };
+    };
+
+    userTags();
+
+    htmlString += "<div class='col-md-12'><a href='#' id='back-btn'>Back to list</a><div id='my-guy' class='main-card'><div class='col-md-4'>" +
+              "<img class='avatar clip-circle ' src='" + myGuy.avatar + "'>" +
+              "</div><div class='col-md-8'>" +
+              "<h3>" + 
+                myGuy.firstName + " " + myGuy.lastName + 
+              "</h3>" + 
+              "<ul><li>"; 
+
+              if (myGuy.phone !== "") {
+                htmlString += "Phone: " + myGuy.phone;
+              } else {
+                htmlString += "Phone number unavailable";
+              };
+                              
+              htmlString += "</li><li>";
+              if (myGuy.email !== "") {
+                htmlString += "Email: " + myGuy.email;
+              } else {
+                htmlString += "Email address unavailable";
+              };
+
+              htmlString += "</li><li>";
+              if (myGuy.address.city !== "") {
+                htmlString += "City: " + myGuy.address.city;
+              } else {
+                htmlString += "Location unavailable";
+              };
+
+              htmlString += "</li><li>";
+              if (myGuy.dob !== "") {
+                htmlString += "Date of Birth: " + myGuy.dob;
+              } else {
+                htmlString += "Date of birth unavailable";
+              };
+
+              htmlString += "</li><li>";
+              if (tags !== "") {
+                htmlString += "Tags: " + tags;
+              } else {
+                htmlString += "Tags unavailable";
+              };
+                                       
+              htmlString += "</li></ul>";
+              
+              htmlString += "</div></div></div>";
+
+
+
+    userContainer.insertAdjacentHTML('beforeend', htmlString);
+
+
+var backBtn = document.getElementById("back-btn");
+backBtn.addEventListener("click", refresher);
+
+    }
+
+var iii = 0;
+
+function cardFunction(e) {
+  
+  if (e.target !== e.currentTarget && iii == 0 && e.target.id !== "") {
+    if (e.target.id == "back-btn") {} else {
+  iii++;
+  var clickedCard = e.target.id;
+  var specUser = "http://challenge-dev.starmarkcloud.com/users/" + clickedCard;         // HOW DOES THIS NOT WORK!!!!!!!!!!!!!!!!!!!!!!!!
+  var specRequest = new XMLHttpRequest();
+  specRequest.open("GET", "http://challenge-dev.starmarkcloud.com/users/1");
+  specRequest.onload = function() {
+    var userData = JSON.parse(ourRequest.responseText);
+    if (specRequest.status >= 200 && specRequest.status < 400) {
+      expandCard(userData, clickedCard);
+    } else {
+      console.log("We connected to the server, but it returned an error.");
+    } 
+  };
+  };
+  
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
+
+specRequest.send();
+
+
+    };
+  }
+
+
+userContainer.addEventListener("click", cardFunction);
+
+
+// Refresh all user cards
+
+
+
+var refresher = function() {
+  var ourData = JSON.parse(ourRequest.responseText);
+  iii--;
+  renderHTML(ourData);
+}
+
+var refresh = document.getElementById("refresh");
+refresh.addEventListener("click", refresher, false);
+
+
+
+
+
 
 
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,26 @@
-# Starmark
+# Starmark Coding Challenge
+
+View my [Starmark Coding Challenge](http://star.cjohn.design/) online.
+
+## Viewing on Localhost
+
+Clone this repo to your harddrive. Spin up MAMP (or the like) and set the root to that directory. Open your browsers and enter `localhost:8888` into the url bar.
+
+## MVP criteria - Achieved
+Ability to filter by city
+Format information based on the provided wireframes
+Information in the DB is inconsistently formatted 
+Available as a list & detail page
+Can be run from local machine without need of a server
+Includes a README file with instructions to setup & run
+The look of the site is secondary to the function for this internal tool
+ADD YOUR OWN SUCCESS CRITERIA - Added refresh button for unfiltering by city
+
+### MVP Criteria - Unsuccessful
+Employees are grouped by last name (First Letter)
+Employee Data is available via API
+
+## Built with:
+[github.com/cjohndesign/_build](https://github.com/cjohndesign/_build)
+
+> (MIT License)
